36d7688efe19c6ba59b4bea06b7b7fa390ac0d47
Olle Ljung <olle.ljung@gmail.com>
2024-06-30 23:06:36 +0000

    Updates all currency pricings

--- run.py
+++ run.py
@@ -8,8 +8,6 @@ load_dotenv()
 
 # -----------------------------------
 #### START CONFIGURATION ####
-# NOTE: This will ONLY update the pricing for the main currency. Please trigger the update for other currencies manually before exposing the product.
-
 
 monthly = 1.45
 COST_PER_UNIT = {
@@ -21,18 +19,17 @@ COST_PER_UNIT = {
     # 'triennially': 19.20
 }
 
-MAIN_CURRENCY_ID = 1 # Assuming id 1 is the main currency. Can be checked by running: SELECT id, code, `default` FROM tblcurrencies;
-
 # To find the configoptionid, to go the Configurable Option Groups section in WHMCS: https://whmcs.example/admin/configproductoptions.php?action=managegroup&id=1
 # Then, click to edit the configurable option in the group you want to set pricing for.
 # The configid appears in the URL of the popup window, and looks like this: https://whmcs.example/admin/configproductoptions.php?manageoptions=true&cid=1479 (cid is the configoptionid)
-CONFIGOPTION_ID = 1
+CONFIGOPTION_ID = 1479
 
-PRODUCT_ID = 1  # Main product ID
+PRODUCT_ID = 56  # Main product ID
 
 MIN_VALUE = 2  # Minimum value
 MAX_VALUE = 24  # Maximum value
 UNIT_TYPE = "GB"  # Unit type: "GB", or any other unit
+
 #------------------------------------
 #### END CONFIGURATION ####
 
@@ -51,27 +48,26 @@ def connect_to_database():
         print(f"Error connecting to MySQL: {e}")
         return None
       
-# def get_main_product_pricing(connection, product_id):
-#     """Retrieve main product pricing for all terms."""
-#     cursor = connection.cursor()
-#     query = """
-#     SELECT monthly, quarterly, semiannually, annually, biennially, triennially
-#     FROM tblpricing
-#     WHERE currency = %s AND relid = %s AND type = 'product'
-#     """
-#     cursor.execute(query, (MAIN_CURRENCY_ID, product_id))
-#     return cursor.fetchone()
+def get_currency_info(connection):
+    cursor = connection.cursor()
+    cursor.execute("SELECT id, rate FROM tblcurrencies")
+    return cursor.fetchall()  # Returns a list of tuples (id, rate)
 
 def configure(connection, configoptionid, min_value, max_value, unit_type):
     """ Replace existing configoptionssubs with a new range and unit type. """
     try:
+        connection.start_transaction()
+        
         cursor = connection.cursor()
+        
+        currency_info = get_currency_info(connection)
+        
         # Delete existing configoptionssubs for the given configoptionid
         delete_sql = "DELETE FROM tblproductconfigoptionssub WHERE configid = %s"
         cursor.execute(delete_sql, (configoptionid,))            
 
         # Insert new configoptionssubs and update pricing
-        insert_sql = """
+        insert_configoptionssub_sql = """
         INSERT INTO tblproductconfigoptionssub (configid, optionname, sortorder, hidden)
         VALUES (%s, %s, %s, %s)
         """
@@ -84,7 +80,7 @@ def configure(connection, configoptionid, min_value, max_value, unit_type):
 
         update_configoptionsub_pricing_sql = """
         INSERT INTO tblpricing (currency, type, relid, monthly, quarterly, semiannually, annually, biennially, triennially, msetupfee, qsetupfee, ssetupfee, asetupfee, bsetupfee, tsetupfee)
-        VALUES (%s, 'configoptions', LAST_INSERT_ID(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        VALUES (%s, 'configoptions', %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
         
         setup_fees = (0,0,0,0,0,0) # Disable setup-fees for now.
@@ -105,8 +101,8 @@ def configure(connection, configoptionid, min_value, max_value, unit_type):
             else:
                 option_name = f"{value}|{value}{unit_type}"
 
-            cursor.execute(insert_sql, (configoptionid, option_name, value, hidden))
-          
+            cursor.execute(insert_configoptionssub_sql, (configoptionid, option_name, value, hidden))
+            last_insert_id = cursor.lastrowid  # Used to update the pricing with multiple currencies
           
             periodic_prices = []
             expected_terms = ['monthly', 'quarterly', 'semiannually', 'annually', 'biennially', 'triennially']
@@ -137,16 +133,19 @@ def configure(connection, configoptionid, min_value, max_value, unit_type):
                 
                 calculated_price = value *  COST_PER_UNIT.get(term, 0) - base_costs[term]
                 periodic_prices.append(calculated_price)
+                
+            # Update the pricing for each currency
+            for currency_id, currency_rate in currency_info:
+                currency_periodic_prices = [price if price == -1 else price * float(currency_rate) for price in periodic_prices]
+                final_prices = tuple(currency_periodic_prices) + setup_fees
+                cursor.execute(update_configoptionsub_pricing_sql, (currency_id, last_insert_id, *final_prices))
 
-            final_prices = tuple(periodic_prices) + setup_fees
-            
-
-            # Set all the configoption prices
-            cursor.execute(update_configoptionsub_pricing_sql, (MAIN_CURRENCY_ID, *final_prices))
             
         # Set the main product prices
-        final_main_product_prices = tuple(main_product_price) + setup_fees
-        cursor.execute(update_mainproduct_pricing_sql, (*final_main_product_prices, MAIN_CURRENCY_ID, PRODUCT_ID))
+        for currency_id, currency_rate in currency_info:
+            currency_main_product_price = [price if price == -1 else price * float(currency_rate) for price in main_product_price]
+            final_main_product_prices = tuple(currency_main_product_price) + setup_fees
+            cursor.execute(update_mainproduct_pricing_sql, (*final_main_product_prices, currency_id, PRODUCT_ID))
 
         connection.commit()
         print(f"Config options and pricing updated for configoptionid {configoptionid} with range {min_value} to {max_value} {unit_type}.")
@@ -157,10 +156,6 @@ def configure(connection, configoptionid, min_value, max_value, unit_type):
 
 if __name__ == "__main__":
     connection = connect_to_database()
-    if connection and connection.is_connected():
-      
-        # main_product_pricing = get_main_product_pricing(connection, PRODUCT_ID)
-        
+    if connection and connection.is_connected():    
         configure(connection, CONFIGOPTION_ID, MIN_VALUE, MAX_VALUE, UNIT_TYPE)
-        
         connection.close()
